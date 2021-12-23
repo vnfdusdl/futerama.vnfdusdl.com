ebb9d572bd41b84960fcd88a3aa0d251eb2f41fa
choyeon <rlachdus2@naver.com>
2021-12-24 02:00:42 +0900

    ✨ episodes API 연동

--- src/pages/episodes.tsx
+++ src/pages/episodes.tsx
@@ -1,8 +1,36 @@
 import type { NextPage } from 'next';
+import { Episode } from '../types/episode';
+import { useFuteramaData } from '../../hooks/useFuteramaData';
+import { Error, Loading } from '../components';
 
-const EpisodePage : NextPage = (page) => {
+const EpisodePage: NextPage = (page) => {
+    const { data, error } = useFuteramaData('episodes');
+
+    if (error) return <Error />;
+    if (!data) return <Loading />;
+
     return (
-        <h1>Episode</h1>
-    )
-}
+        <div>
+            <h1>Episode</h1>
+            <main>
+                {data.map((EpisodeData: Episode) => {
+                    const { number, title, writers, originalAirDate, desc, id } = EpisodeData;
+                    return (
+                        <div key={`episode-list-${id}`}>
+                            <article>
+                                <h2>{title}</h2>
+                                <p>작가 : {writers}</p>
+                                <p>방영일 : {originalAirDate}</p>
+                                <div>
+                                    <span>줄거리</span>
+                                    <p>{desc}</p>
+                                </div>
+                            </article>
+                        </div>
+                    );
+                })}
+            </main>
+        </div>
+    );
+};
 export default EpisodePage;
