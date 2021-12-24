76fb0dc563780136337163fb7a6605f5acf710ac
choyeon <rlachdus2@naver.com>
2021-12-24 21:50:00 +0900

    ✨ inventory API 연결

--- src/pages/inventory.tsx
+++ src/pages/inventory.tsx
@@ -1,8 +1,35 @@
 import type { NextPage } from 'next';
+import { InventoryData } from '../types/inventory';
+import { useFuteramaData } from '../../hooks/useFuteramaData';
+import { Error, Loading } from '../components';
 
-const InventoryPage : NextPage = (page) => {
+const InventoryPage: NextPage = (page) => {
+    const { data, error } = useFuteramaData('inventory');
+
+    if (error) return <Error />;
+    if (!data) return <Loading />;
+
     return (
-        <h1>iventory</h1>
-    )
-}
+        <div>
+            <h1>Cast</h1>
+            <main>
+                {data.map((InventoryData: InventoryData) => {
+                    const { title, category, description, slogan, price, stock, id } = InventoryData;
+                    return (
+                        <div key={`inventory-list-${id}`}>
+                            <article>
+                                <h2>{title}</h2>
+                                <p>{category}</p>
+                                <p>description</p>
+                                <p>slogan</p>
+                                <p>price</p>
+                                <p>stock</p>
+                            </article>
+                        </div>
+                    );
+                })}
+            </main>
+        </div>
+    );
+};
 export default InventoryPage;
