0988af0cf53f3d7c8f50d1fa4fe6feafc3e38878
choyeon <rlachdus2@naver.com>
2021-12-25 17:20:49 +0900

    ✨ Layout 구현

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,34 @@
 export const FUTERAMA_API_ENDPOINT = 'https://api.sampleapis.com/futurama/';
+
+export const ROUTES = [
+    {
+    ID : 0,
+    PATH: '.',
+    LABEL: 'Information'
+    },
+    {
+    ID : 1,
+    PATH: '/characters',
+    LABEL: 'characters'
+    },
+    {
+    ID : 2,
+    PATH: '/cast',
+    LABEL: 'cast'
+    },
+    {
+        ID : 3,
+        PATH: '/episodes',
+        LABEL: 'episodes'
+    },
+    {
+        ID : 4,
+        PATH: '/questions',
+        LABEL: 'questions'
+    },
+    {
+        ID : 5,
+        PATH: '/inventory',
+        LABEL: 'inventory'
+    }
+]
